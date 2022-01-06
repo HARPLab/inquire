fixed performance scaling and error bars

tests/evaluation.py
@@ -7,7 +7,7 @@ class Evaluation:
     @staticmethod
     def run(domain, teacher, agent, num_tasks, num_runs, num_queries, num_test_states, verbose=False):
         rand = RandomState(0)
-        perf_mat = np.zeros((num_tasks,num_runs,num_queries))
+        perf_mat = np.zeros((num_tasks,num_runs*num_test_states,num_queries))
         dist_mat = np.zeros((num_tasks,num_runs,num_queries))
         if verbose:
             print("Initializing tasks...")
@@ -23,9 +23,11 @@ class Evaluation:
             if verbose:
                 print("Finding optimal baselines...")
             for test_state in task.test_states:
-                optimal_traj = task.optimal_trajectory_from_ground_truth(test_state)
-                optimal_reward = task.ground_truth_reward(optimal_traj)
-                test_set.append([test_state, optimal_traj, optimal_reward])
+                best_traj = task.optimal_trajectory_from_ground_truth(test_state)
+                max_reward = task.ground_truth_reward(best_traj)
+                worst_traj = task.least_optimal_trajectory_from_ground_truth(test_state)
+                min_reward = task.ground_truth_reward(worst_traj)
+                test_set.append([test_state, (worst_traj, best_traj), (min_reward, max_reward)])
 
             ## Reset learning agent for each run and iterate through queries
             if verbose:
@@ -46,10 +48,12 @@ class Evaluation:
                     w_mean = np.mean(w_dist, axis=0)
 
                     ## Get performance metrics after weight update
-                    perf = 0.0
-                    for test_case in test_set:
-                        model_traj = domain.optimal_trajectory_from_w(test_case[0], w_mean)
-                        perf_mat[t,r,k] += task.ground_truth_reward(model_traj)/test_case[2]
-                    perf_mat[t,r,k] = perf_mat[t,r,k]/float(len(test_set))
+                    for c in range(num_test_states):
+                        model_traj = domain.optimal_trajectory_from_w(test_set[c][0], w_mean)
+                        reward = task.ground_truth_reward(model_traj)
+                        min_r, max_r = test_set[c][2]
+                        perf = (reward - min_r) / (max_r - min_r)
+                        assert 0 <= perf <= 1
+                        perf_mat[t,(r*num_runs)+c,k] = perf
                     dist_mat[t,r,k] = task.distance_from_ground_truth(w_mean)
         return perf_mat, dist_mat

tests/icml22.py
@@ -22,13 +22,10 @@ def plot_results(results, labels, filename):
         for a in range(task_mat.shape[1]):
             series = np.transpose(task_mat[t,a])
             label = labels[a]
-            x = [i+1 for i in range(series.shape[0])]
-            mean = np.mean(series,axis=1)
-            if series.shape[0] > 1:
-                std = np.std(series,axis=1)
-            else:
-                std = np.zeros_like(mean)
-            plt.errorbar(x, mean, yerr=std, color=colors[a%len(colors)],label=label)
+            x = [i+1+(0.05*a) for i in range(series.shape[0])]
+            med = np.median(series,axis=1)
+            err = abs(np.percentile(series,(25,75),axis=1)-med)
+            plt.errorbar(x, med, fmt='.-', yerr=err, color=colors[a%len(colors)],label=label)
         plt.legend(labels)
         plt.xticks(np.arange(1, task_mat.shape[-1]+1, 1.0))
         plt.savefig(output_dir + filename + "-task_" + str(t) + ".png")
@@ -82,8 +79,8 @@ if __name__ == '__main__':
         demo_agent = Inquire(sampling_method, sampling_params, args.num_w_samples, args.num_traj_samples, traj_length, [Demonstration])
         pref_agent = Inquire(sampling_method, sampling_params, args.num_w_samples, args.num_traj_samples, traj_length, [Preference])
         corr_agent = Inquire(sampling_method, sampling_params, args.num_w_samples, args.num_traj_samples, traj_length, [Correction])
-        agents = [inquire_agent, demo_agent, pref_agent] #, corr_agent]
-        agent_names = ["INQIRE", "Demo-only", "Pref-only"] #, "Corr-only"]
+        agents = [demo_agent, pref_agent] #, inquire_agent, corr_agent]
+        agent_names = ["Demo-only", "Pref-only"] #, "INQUIRE", "Corr-only"]
     if args.agent_name == "inquire":
         agents = [Inquire(sampling_method, sampling_params, args.num_w_samples, args.num_traj_samples, traj_length, [Demonstration, Preference])]
         agent_names = ["INQUIRE"]