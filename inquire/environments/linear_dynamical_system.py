2b3c9f5285cb8c9e35cba52e7f09a5b94b574554
Peej1818 <peej1818@gmail.com>
2022-04-18 11:49:43 -0400

    More LDS work.

--- inquire/environments/linear_dynamical_system.py
+++ inquire/environments/linear_dynamical_system.py
@@ -48,7 +48,7 @@ class LinearDynamicalSystem(Environment):
             ::trajectory_length: The number of discrete states and controls
                                  in a trajectory. Note the term 'trajectory' is
                                  used for interpretability; 'sequence' might be
-                                 a more apt term.
+                                 a more apt term in this context.
             ::control_space_discretization: How many discrete actions to
                                             generate from (-1,1) continuous
                                             control bounds.
@@ -63,7 +63,7 @@ class LinearDynamicalSystem(Environment):
         self._verbose = verbose
 
         # Initialize state with zeros:
-        self._state = np.zeros(size=(state_vector_size, 1))
+        self._state = np.zeros((state_vector_size, 1))
         # Randomly select goal state:
         self._goal_state = self._rng.integers(
             low=0, high=100, size=(self._state_vector_size, 1)
@@ -71,11 +71,17 @@ class LinearDynamicalSystem(Environment):
         self._controls_bounds = np.array([[-1, 1]]).repeat(
             state_vector_size, axis=0
         )
+        self._lower_bound = [
+            x[0] for x in self.control_bounds
+        ] * self._trajectory_length
+        self._upper_bound = [
+            x[1] for x in self.control_bounds
+        ] * self._trajectory_length
         # Apply a control to each state-element for trajectory_duration time:
-        self._controls_vector_full_trajectory = np.array(
+        self._controls_vector = np.array(
             [self._trajectory_duration, self._state_vector_size]
         )
-        self._controls_vector = np.full(
+        self._controls_matrix = np.full(
             (self._state_vector_size, control_space_discretization),
             fill_value=np.linespace(-1, 1, control_space_discretization),
         )
@@ -90,15 +96,30 @@ class LinearDynamicalSystem(Environment):
             )
             return
 
-    def generate_random_state(self, random_state):
-        """Generate random start and goal states between (0, 1)."""
-        self._state = random_state.random(size=self._state_vector_size)
-        self._goal_state = random_state.random(size=self._state_vector_size)
-        return
+    def reset(self, seed: int = None) -> None:
+        """Reset the environment according to seed.
 
-    def generate_random_reward(self, random_state):
-        """Generate random configuration of weights between (0, 1)."""
-        generated = random_state.random(size=self._state_vector_size)
+        Note: Start state is always the 0-vector.
+        """
+        self._state = np.zeros((self._state_vector_size, 1))
+        if seed is None:
+            self._rng = np.random.default_rng(self._seed)
+        else:
+            self._rng = np.random.default_rng(seed)
+        self._goal_state = self._rng.integers(
+            low=0, high=100, size=(self._state_vector_size, 1)
+        )
+
+    def generate_random_state(self, random_state) -> np.ndarray:
+        """Generate random goal as state vector."""
+        rando = self._rng.integers(
+            low=0, high=100, size=(self._state_vector_size, 1)
+        )
+        return rando
+
+    def generate_random_reward(self, random_state) -> np.ndarray:
+        """Generate weights of random value between (0, 1)."""
+        generated = self._rng.random(size=(self._number_of_features, 1))
         return generated
 
     def features(self, action: np.ndarray, state: np.ndarray) -> np.ndarray:
@@ -109,14 +130,14 @@ class LinearDynamicalSystem(Environment):
         """
         next_state = self.next_state(state, action)
 
-        distance_from_goal_state = np.exp(
-            -(np.abs(next_state - self._goal_state))
+        latest_features = np.empty(
+            (self._number_of_features, self._state_vector_size)
         )
-        latest_features = np.empty((self.number_of_features, 1))
 
-        # Extract features from next_state:
+        # Extract features--a vector of order-i polynomials:
         for i, f in enumerate(self._number_of_features):
-            latest_features[i, 0] = distance_from_goal_state ** i
+            latest_features[i, :] = next_state ** i
+            # latest_features[i, :] = next_state
         return latest_features
 
     def run(self, controls: np.ndarray) -> np.ndarray:
@@ -130,36 +151,44 @@ class LinearDynamicalSystem(Environment):
         for i, t in enumerate(time_adjusted_controls):
             self._state = self._state + t
             trajectory[i, :] = self._state
-            # See if we've reached the goal:
-            if np.all(trajectory[i, :] == self._goal_state):
-                # Reached the goal in fewer controls than provided; adjust
+            if self.is_terminal_state(trajectory[i, :]):
+                # Reached goal in fewer controls than provided; adjust
                 # accordingly:
                 trajectory = trajectory[: i + 1, :]
                 time_adjusted_controls = time_adjusted_controls[: i + 1, :]
-                break
+                return [trajectory, time_adjusted_controls]
         return [trajectory, time_adjusted_controls]
 
     def optimal_trajectory_from_w(self, start_state, w):
-        """Compute the optimal trajectory to goal given weights w."""
+        """Compute the optimal trajectory to goal given weights w.
 
-        def reward_fn(controls: np.ndarray, weights: np.ndarray) -> float:
+        ::inputs:
+            ::start_state: A seed for the environment resets.
+            ::w: A set of weights.
+        """
+        self._seed = start_state
+        self.reset(start_state)
+
+        def reward_fn(
+            controls: np.ndarray, domain: Environment, weights: np.ndarray
+        ) -> float:
             """Return reward for given controls and weights."""
             trajectory = self.run(controls)
-            features = np.empty((self._number_of_features, 1))
+            features = np.empty(
+                (self._number_of_features, self._state_vector_size)
+            )
             for i, s in enumerate(trajectory):
-                features[i, 0] += self.features(
-                    trajectory[0][i, :], trajectory[1][i, :]
+                features[i, :] += self.features(
+                    trajectory[1][i, :], trajectory[0][i, :]
                 )
 
-            features = features.mean()
-            rwd = -weights.T @ features
-            return rwd
+            # features = features.sum()
+            rwd = 0
+            for j, w in enumerate(weights):
+                rwd += np.sum(w * features[j, :])
+            # rwd = -weights.T @ features
+            return -rwd
 
-        self._seed = start_state
-        self._state = self.known_reset(start_state)
-
-        low = [x[0] for x in self.control_bounds] * self._trajectory_length
-        high = [x[1] for x in self.control_bounds] * self._trajectory_length
         optimal_ctrl = None
         opt_val = np.inf
         start = time.perf_counter()
@@ -173,15 +202,15 @@ class LinearDynamicalSystem(Environment):
             temp_result = opt.fmin_l_bfgs_b(
                 reward_fn,
                 x0=np.random.uniform(
-                    low=low,
-                    high=high,
-                    size=self.control_size * trajectory_length,
+                    low=self._lower_bound,
+                    high=self._upper_bound,
+                    size=self._state_vector_size * self._trajectory_length,
                 ),
-                args=(self, weights),
-                bounds=self.control_bounds * trajectory_length,
+                args=(self, w),
+                bounds=self._state_vector_size * self._trajectory_length,
                 approx_grad=True,
-                # maxfun=1000,
-                # maxiter=100,
+                maxfun=1000,
+                maxiter=100,
             )
             if temp_result[1] < opt_val:
                 optimal_ctrl = temp_result[0]
@@ -198,7 +227,6 @@ class LinearDynamicalSystem(Environment):
             {
                 "controller_1": optimal_trajectory[1][:, 0],
                 "controller_2": optimal_trajectory[1][:, 1],
-                "state seed": start_state,
                 "state_0": optimal_trajectory[0][:, 0],
                 "state_1": optimal_trajectory[0][:, 1],
                 "state_2": optimal_trajectory[0][:, 2],
@@ -206,46 +234,41 @@ class LinearDynamicalSystem(Environment):
                 "state_4": optimal_trajectory[0][:, 4],
                 "state_5": optimal_trajectory[0][:, 5],
                 "state_6": optimal_trajectory[0][:, 6],
-                "state_7": optimal_trajectory[0][:, 7],
             }
         )
-        # current_time = time.localtime()
-        # TODO Consider pickling:
-        # if self.verbose:
-        #    print("Saving trajectory ...")
-        # df.to_csv(
-        #    self.output_path
-        #    + time.strftime("%m:%d:%H:%M:%S_", current_time)
-        #    + f"_weights_{weights[0]:.2f}_{weights[1]:.2f}_{weights[2]:.2f}_{weights[3]:.2f}_"
-        #    + ".csv"
-        # )
+        current_time = time.localtime()
+        if self.verbose:
+            print("Saving trajectory ...")
+        df.to_csv(
+            self.output_path
+            + time.strftime("%m:%d:%H:%M:%S_", current_time)
+            + f"_weights_{w[0]:.2f}_{w[1]:.2f}_{w[2]:.2f}_{w[3]:.2f}_"
+            + ".csv"
+        )
 
         # Extract the features from that optimal trajectory:
-        features = np.zeros((optimal_trajectory[0].shape[0], self.w_dim))
+        features = np.zeros(
+            (optimal_trajectory[0].shape[0], self._number_of_features)
+        )
         for i in range(optimal_trajectory[0].shape[0]):
             if i + 1 == optimal_trajectory[0].shape[0]:
-                f = self.feature_fn(
-                    optimal_trajectory[1][i],
-                    optimal_trajectory[0][i],
-                    at_last_state=True,
+                f = self.features(
+                    optimal_trajectory[1][i], optimal_trajectory[0][i]
                 )
             else:
-                f = self.feature_fn(
+                f = self.feature(
                     optimal_trajectory[1][i], optimal_trajectory[0][i]
                 )
             features[i, :] = f
 
-        # Set the initial velocity equal to 0:
-        features[0, -2] = 0
-        phi_total = np.mean(features[:, :-1], axis=0)
-        phi_total = np.append(phi_total, features[-1, -1])
-        optimal_trajectory_final = Trajectory(optimal_trajectory, phi_total)
-        self.state = self.known_reset(start_state)
+        phi = np.mean(features, axis=0)
+        optimal_trajectory_final = Trajectory(optimal_trajectory, phi)
+        self.state = self.reset(start_state)
         return optimal_trajectory_final
 
     def all_actions(self):
         """Return continuous action-space."""
-        return self._controls_vector
+        return self._controls_matrix
 
     def available_actions(self, current_state):
         """Return the actions available in current_state."""
@@ -257,13 +280,17 @@ class LinearDynamicalSystem(Environment):
     def next_state(self, current_state, action):
         """Return effect on state if action taken in current_state."""
         if action.shape != self._controls_vector.shape:
-            action_all_dimensions = np.full_like(self._controls_vector, action)
-        next_state = self._state + action_all_dimensions
+            action_all_dimensions = np.full_like(
+                self._controls_vector, action.reshape(1, -1)
+            )
+            next_state = current_state + action_all_dimensions
+        else:
+            next_state = current_state + action
         return next_state
 
     def is_terminal_state(self, current_state):
         """Check if current_state signals execution completion."""
-        if np.isclose(current_state, self._state):
+        if np.all(np.isclose(current_state, self._goal_state)):
             return True
         else:
             return False
