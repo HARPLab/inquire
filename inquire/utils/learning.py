f3a91bc3a3fb16159e7ee67cc5b386b88cbc4d3e
Peej1818 <peej1818@gmail.com>
2022-05-11 10:55:33 -0400

    Added some plotting functionality to analyze gradients

--- inquire/utils/learning.py
+++ inquire/utils/learning.py
@@ -2,6 +2,8 @@ import pdb
 import time
 import numpy as np
 
+import plotly.express as px
+
 class Learning:
     @staticmethod
     def discrete_q_iteration(domain, start_state, w, threshold=1e-4, discount=0.99):
@@ -32,8 +34,11 @@ class Learning:
         return values
 
     @staticmethod
-    def gradient_descent(rand, feedback, gradient_fn, w_dim, sample_count, learning_rate=0.0025, conv_threshold=1.0e-5, viz=True):
+    def gradient_descent(rand, feedback, gradient_fn, w_dim, sample_count, learning_rate=0.0005, conv_threshold=1.0e-5, viz=True):
         samples = []
+        norms = []
+        weights_at_lowest = []
+        lowest = np.inf
         start = time.perf_counter()
         for _ in range(sample_count):
             init_w = rand.uniform(-1,1,w_dim) #.reshape(-1,1)
@@ -44,13 +49,38 @@ class Learning:
                 grads = gradient_fn(feedback, curr_w)
                 new_w = curr_w - (learning_rate * np.array(grads))
                 new_w = new_w/np.linalg.norm(new_w)
-                if np.linalg.norm(new_w - curr_w) < conv_threshold:
+                norms.append(np.linalg.norm(new_w - curr_w))
+                if norms[-1] < lowest:
+                    weights_at_lowest = new_w
+                    lowest = norms[-1]
+                # if np.linalg.norm(new_w - curr_w) < conv_threshold:
+                if norms[-1] < conv_threshold:
                     converged = True
+                    norms = []
+                    curr_w = weights_at_lowest
+                    weights_at_lowest = []
+                    lowest = np.inf
+                    start = time.perf_counter()
                 curr_w = new_w
-                # print(f"G.D. weights:\n{new_w}.")
                 elapsed = time.perf_counter() - start
-                if elapsed >= 120:
+                if elapsed >= 90:
+                    divisor = 1
                     print("Timeout on gradient descent.")
+                    for i in range(2, 1000):
+                        if len(norms) % i == 0:
+                            divisor = i
+                    avg_norms = np.array(norms).reshape(np.max((divisor, int(len(norms)/divisor))), np.min((divisor, int(len(norms)/divisor)))).mean(axis=1)
+                    fig = px.line(
+                            x=np.arange(avg_norms.shape[0]),
+                            y=avg_norms,
+                            title=f"{len(norms)} gradient iterations."
+                    )
+                    fig.show()
+                    norms = []
+                    curr_w = weights_at_lowest
+                    print(f"Lowest norm was: {lowest}\nWeights at that point were: {curr_w}.")
+                    weights_at_lowest = []
+                    lowest = np.inf
                     start = time.perf_counter()
                     break
             samples.append(curr_w)
