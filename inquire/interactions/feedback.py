63cae864d347b9c7ba4508ea11bd25abd77eedf8
Peej1818 <peej1818@gmail.com>
2022-05-12 08:30:41 -0400

    Added some type-hints for clarity.

--- inquire/interactions/feedback.py
+++ inquire/interactions/feedback.py
@@ -1,13 +1,23 @@
+from typing import Union
+
+import numpy as np
+
 class Query:
-    def __init__(self, query_type, task, start_state, trajectories):
+    def __init__(
+            self,
+            query_type: str,
+            task: object,
+            start_state: Union[list, np.ndarray],
+            trajectories: list
+    ):
         self.query_type = query_type
-        self.task = task  # A task has a domain as an instance attribute
+        self.task = task  # A task has an Environment as an instance attribute
         self.start_state = start_state
         self.trajectories = trajectories
 
 
 class Trajectory:
-    def __init__(self, trajectory, phi):
+    def __init__(self, trajectory: list, phi: Union[list, np.ndarray]):
         self.trajectory = trajectory
         self.phi = phi
 
