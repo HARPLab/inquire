changed gradient function to fix demo vs pref weighting. revised teacher corrections.

inquire/agents/inquire.py
@@ -62,9 +62,12 @@ class Inquire(Agent):
     def gradient(feedback, w):
         grads = np.zeros_like(w)
         for fb in feedback:
-            phis = np.array([f.phi for f in fb.options])
-            exps = np.exp(np.dot(phis,w)).reshape(-1,1)
-            grads = grads + (fb.selection.phi - ((exps*phis).sum(axis=0)/exps.sum()))
+            phi_pos = fb.selection.phi
+            for f in fb.options:
+                if any(f.phi != phi_pos):
+                    phis = np.array([f.phi, phi_pos])
+                    exps = np.exp(np.dot(phis,w)).reshape(-1,1)
+                    grads = grads + (fb.selection.phi - ((exps*phis).sum(axis=0)/exps.sum()))
         return grads * -1
 
     @staticmethod
@@ -76,6 +79,10 @@ class Inquire(Agent):
 
     @staticmethod
     def generate_prob_mat(exp, int_type): #|Q| x |C| x |W|
+        #if int_type is Demonstration:
+        #    choice_matrix = np.expand_dims(np.array(list(range(exp.shape[0]))),axis=0)
+        #    mat = exp / (exp + np.transpose(exp,(1,0,2)))
+        #    return np.expand_dims(np.prod(mat, axis=1), axis=0), choice_matrix
         if int_type is Demonstration:
             choice_matrix = np.expand_dims(np.array(list(range(exp.shape[0]))),axis=0)
             return np.expand_dims(exp[0] / np.sum(exp, axis=1), axis=0), choice_matrix
@@ -112,7 +119,7 @@ class Inquire(Agent):
                 print("Assessing " + str(i.__name__) + " queries...")
             prob_mat, choice_idxs = Inquire.generate_prob_mat(exp_mat, i)
             gains = Inquire.generate_gains_mat(prob_mat, self.M)
-            query_gains = np.sum(gains, axis=(1,2))
+            query_gains = np.mean(np.sum(gains, axis=-1), axis=-1)
             all_gains.append(query_gains)
             all_queries.append(choice_idxs)
         if verbose:

inquire/environments/linear_combo.py
@@ -8,9 +8,6 @@ class LinearCombination(Environment):
         self._seed = seed
         self._rng = np.random.default_rng(self._seed)
         self.w_dim = w_dim
-        #density = 10 #number of samples per dimension
-        #samples = self._rng.normal(0, 1, (self.w_dim, density**self.w_dim))
-        #self.w_samples = samples / np.linalg.norm(samples,axis=0)
 
     def generate_random_state(self, random_state):
         return np.zeros(self.w_dim)
@@ -52,3 +49,5 @@ class LinearCombination(Environment):
     def trajectory_from_states(self, states, features):
         return Trajectory(states, np.sum(features, axis=0))
 
+    def distance_between_trajectories(self, a, b):
+        return np.linalg.norm(a.trajectory[-1][1] - b.trajectory[-1][1])

inquire/teachers/optimal.py
@@ -78,10 +78,29 @@ class OptimalTeacher(Teacher):
         return Choice(traj, [traj] + query.trajectories)
 
     def preference(self, query: Query) -> Choice:
-        r = [np.dot(query.task.get_ground_truth(), qi.phi) for qi in query.trajectories]
+        r = [query.task.ground_truth_reward(qi) for qi in query.trajectories]
         return Choice(selection=query.trajectories[np.argmax(r)], options=query.trajectories)
 
     def correction(self, query: Query) -> Choice:
+        alpha = 1.0 #optional parameter for discouraging more distanced corrections
+        rewards, dists = [], []
+        traj_samples = TrajectorySampling.uniform_sampling(query.start_state, None, query.task.domain, np.random.RandomState(0), self._steps, self._N, {'remove_duplicates': True})
+        t_query = query.trajectories[0]
+        for t in traj_samples:
+            t_reward = query.task.ground_truth_reward(t)
+            rewards.append(t_reward)
+            t_dist = query.task.domain.distance_between_trajectories(t, t_query)
+            dists.append(t_dist)
+        min_r = query.task.ground_truth_reward(t_query)
+        max_r = np.max(np.array(rewards))
+        scaled_rewards = np.array([(r - min_r) / (max_r - min_r) for r in rewards])
+        dists = np.array(dists)
+        scaled_dists = np.exp(alpha * dists) / np.max(np.exp(alpha * dists))
+        ratios = scaled_rewards / scaled_dists
+        correction = traj_samples[np.argmax(ratios)]
+        return Choice(selection=correction, options=[correction, t_query])
+
+    def old_correction(self, query: Query) -> Choice:
         curr_state = query.start_state
         feats = [query.task.domain.features(None,curr_state)]
         values = inquire.utils.learning.Learning.discrete_q_iteration(query.task.domain, query.start_state, query.task.get_ground_truth())