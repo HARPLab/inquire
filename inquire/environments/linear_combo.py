97c1c84d955842922097447354639f96890956e3
TescaF <tesca@cmu.edu>
2022-06-08 13:42:51 -0400

    updated linear combo to work with new environment definition

--- inquire/environments/linear_combo.py
+++ inquire/environments/linear_combo.py
@@ -1,5 +1,5 @@
 from inquire.environments.environment import Environment
-from inquire.utils.datatypes import Trajectory
+from inquire.utils.datatypes import Trajectory, Range
 import numpy as np
 import math
 import pdb
@@ -8,48 +8,46 @@ class LinearCombination(Environment):
     def __init__(self, seed, w_dim):
         self._seed = seed
         self._rng = np.random.default_rng(self._seed)
-        self.w_dim = w_dim
+        self.feat_dim = w_dim
+        self.action_rang = Range(
+            np.array([-1]*w_dim),
+            np.ones((w_dim)),
+            np.array([1]*w_dim),
+            np.ones((w_dim))
+        )
+        self.state_rang = self.action_rang
+
+    def w_dim(self):
+        return self.feat_dim
+
+    def action_space(self):
+        return self.action_rang
+
+    def state_space(self):
+        return self.state_rang
 
     def generate_random_state(self, random_state):
-        return np.zeros(self.w_dim)
+        return np.zeros(self.feat_dim)
 
     def generate_random_reward(self, random_state):
-        generated = self._rng.normal(0, 1, size=(self.w_dim,))
+        generated = self._rng.normal(0, 1, size=(self.feat_dim,))
         generated = generated / np.linalg.norm(generated)
         return generated
 
     def optimal_trajectory_from_w(self, start_state, w):
-        return Trajectory([w], w)
+        return Trajectory(states=np.expand_dims(w,axis=0), actions=None, phi=w)
 
-    def features(self, action, state):
-        return state
+    def trajectory_rollout(self, start_state, actions):
+        action = actions[-self.feat_dim:]
+        state = action/np.linalg.norm(action)
+        return Trajectory(states=np.expand_dims(state,axis=0), actions=None, phi=state)
 
-    def available_actions(self, current_state):
-        sample = self._rng.normal(0, 1, (self.w_dim,))
-        sample = sample / np.linalg.norm(sample,axis=0)
-        return [sample]
-
-    def next_state(self, current_state, action):
-        return action
-
-    def is_terminal_state(self, current_state):
-        return np.any(current_state != 0)
-    
-    def all_actions(self):
-        return None
-
-    def state_space_dim(self):
-        return None
-
-    def state_space(self):
-        return None
-
-    def state_index(self, state):
-        return None
-
-    def trajectory_from_states(self, states, features):
-        return Trajectory(states, np.sum(features, axis=0))
+    def features_from_trajectory(self, trajectory):
+        return trajectory.states[-1]
 
     def distance_between_trajectories(self, a, b):
         cos = np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
         return np.arccos(cos) / math.pi
+
+    def visualize_trajectory(self, start_state, trajectory):
+        print(trajectory.states[-1])
