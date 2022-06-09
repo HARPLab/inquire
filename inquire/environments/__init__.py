854f015c46efbb459a03c2b6c7657713a0a579c2
Peej1818 <peej1818@gmail.com>
2022-06-09 07:23:17 -0400

    Removed gym wrapper from environments/__init__.

--- inquire/environments/__init__.py
+++ inquire/environments/__init__.py
@@ -1,6 +1,5 @@
 from inquire.environments.environment import Task, Environment
 from inquire.environments.puddleworld import PuddleWorld
-from inquire.environments.gym_wrapper_environment import GymWrapperEnvironment
 from inquire.environments.linear_combo import LinearCombination
 from inquire.environments.linear_dynamical_system import LinearDynamicalSystem
 from inquire.environments.lunar_lander import LunarLander
@@ -10,7 +9,6 @@ __all__ = [
     "PuddleWorld",
     "Task",
     "Environment",
-    "GymWrapperEnvironment",
     "LinearCombination",
     "LinearDynamicalSystem",
     "LunarLander",
