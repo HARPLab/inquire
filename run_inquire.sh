9e86f4a82edde5ff8db27cb262d9f9c9035aa57d
TescaF <tesca@cmu.edu>
2022-06-12 15:14:08 -0400

    separated out weighted and unweighted scripts

--- run_inquire.sh
+++ run_inquire.sh
@@ -8,10 +8,6 @@ else
     cache=""
     agent="no-demos"
 fi
-demo_cost=20.0
-pref_cost=10.0
-corr_cost=15.0
-bnry_cost=5.0
 demo_beta=50.0
 pref_beta=20.0
 corr_beta=20.0
@@ -20,10 +16,10 @@ conv=0.01
 for a in $alphas; do
     name="inquire--${domain}_alpha-${a}"
     echo $name
-    python tests/corl22.py --domain ${domain} $cache -I 1000 -N 1000 --queries 20 --tests 10 --runs 10 --alpha $a --convergence_threshold $conv --output_name $name --betas "{Modality.DEMONSTRATION: $demo_beta, Modality.PREFERENCE: $pref_beta, Modality.CORRECTION: $corr_beta, Modality.BINARY: $bnry_beta}" --agent ${agent} --costs "{Modality.DEMONSTRATION: $demo_cost, Modality.PREFERENCE: $pref_cost, Modality.CORRECTION: $corr_cost, Modality.BINARY: $bnry_cost}"
+    python tests/corl22.py --domain ${domain} $cache -I 1000 -N 1000 --queries 20 --tests 10 --runs 10 --alpha $a --convergence_threshold $conv --output_name $name --betas "{Modality.DEMONSTRATION: $demo_beta, Modality.PREFERENCE: $pref_beta, Modality.CORRECTION: $corr_beta, Modality.BINARY: $bnry_beta}" --agent ${agent} 
     if [ "$domain" != "linear_combo" ]; then
         name="inquire--static_${domain}_alpha-${a}"
         echo $name
-        python tests/corl22.py --domain ${domain} $cache -I 1000 -N 1000 --queries 20 --tests 10 --runs 10 --alpha $a --convergence_threshold $conv --output_name $name --betas "{Modality.DEMONSTRATION: $demo_beta, Modality.PREFERENCE: $pref_beta, Modality.CORRECTION: $corr_beta, Modality.BINARY: $bnry_beta}" --static_state --agent ${agent} --costs "{Modality.DEMONSTRATION: $demo_cost, Modality.PREFERENCE: $pref_cost, Modality.CORRECTION: $corr_cost, Modality.BINARY: $bnry_cost}"
+        python tests/corl22.py --domain ${domain} $cache -I 1000 -N 1000 --queries 20 --tests 10 --runs 10 --alpha $a --convergence_threshold $conv --output_name $name --betas "{Modality.DEMONSTRATION: $demo_beta, Modality.PREFERENCE: $pref_beta, Modality.CORRECTION: $corr_beta, Modality.BINARY: $bnry_beta}" --static_state --agent ${agent}
     fi
 done
