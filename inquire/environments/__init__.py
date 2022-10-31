2287130c39e01f09ddb882cc96d64be24fc62fcd
TescaF <tesca.fitzgerald@yale.edu>
2022-10-31 14:28:20 -0400

    removed old imports

--- inquire/environments/__init__.py
+++ inquire/environments/__init__.py
@@ -2,8 +2,6 @@ from inquire.environments.environment import Environment, Task
 from inquire.environments.linear_combo import LinearCombination
 from inquire.environments.linear_dynamical_system import LinearDynamicalSystem
 from inquire.environments.lunar_lander import LunarLander
-from inquire.environments.pats_linear_dynamical_system import \
-    PatsLinearDynamicalSystem
 from inquire.environments.pizza_making import PizzaMaking
 from inquire.environments.puddleworld import PuddleWorld
 
@@ -13,7 +11,6 @@ __all__ = [
     "Task",
     "Environment",
     "LinearCombination",
-    "PatsLinearDynamicalSystem",
     "LinearDynamicalSystem",
     "LunarLander",
     "PizzaMaking",
